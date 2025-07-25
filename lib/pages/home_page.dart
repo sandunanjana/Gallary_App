36233dd85ec12fe691c7a6147d305f240dcea878
Sadun Anjana <sadunanjana2003@gmail.com>
2025-07-26 01:30:44 +0530

    creat a home page

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(child: SafeArea(child: Column(
+        children: [
+          Text("Awesome"),
+          Text("Awesome"),
+        ],
+      ))),
+    );
+  }
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/cupertino.dart';
+
+final mainTextColor = Color(0xFF3B3636); 
+const mainColor = Color(0xFF3B3636); 
